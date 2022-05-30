5b9d9c4b5ca1f563f65e7ed28a4ebe654adc64db
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-05-30 18:48:00 +0100

    changeQuoteAndAuthor function added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import { useState } from 'react';
+import React from 'react';
 import './App.scss';
 
 function App() {
-  let quote = "Don't let past mistakes make you lose hope of achieving good. Some of those with the worst past have made a great future for themselves.";
-  let author = "Mufti Menk";
+  const [quote, setQuote] = useState("Don't let past mistakes make you lose hope of achieving good. Some of those with the worst past have made a great future for themselves.");
+  const [author, setAuthor] = useState("Mufti Menk");
+  let newQuote = "Obstacles on our path are some of the tests of life. They make us stronger, better people and open up doors we had never imagined.";
+  let newAuthor = "Ibrahim Menk";
+
+  const changeQuoteAndAuthor = () => {
+    setQuote(newQuote);
+    setAuthor(newAuthor);
+  }
   return (
-    
     <div className="App">
       <header className="App-header">
         <p>
-          "{quote}"
+        “{quote}”
         </p>
         <p>- {author}</p>
+        <button onClick = {changeQuoteAndAuthor}>Change Quote</button>
       </header>
     </div>
   );
