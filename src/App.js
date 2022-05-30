7d4c5dc76987c2c3d1bf39eca0524385c0337b9f
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-05-30 17:45:29 +0100

    Demo quote.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
+import './App.scss';
 
 function App() {
+  let quote = "Don't let past mistakes make you lose hope of achieving good. Some of those with the worst past have made a great future for themselves.";
+  let author = "Mufti Menk";
   return (
+    
     <div className="App">
       <header className="App-header">
         <p>
-          Hello World!
+          "{quote}"
         </p>
+        <p>- {author}</p>
       </header>
     </div>
   );
